e2d98f17d5ca9559ab3f807753cb8c322ec6630a
Miłosz Smółka <m110@m110.pl>
2018-01-23 21:09:08 +0100

    Fix broker unit tests

--- pkg/broker/broker_test.go
+++ pkg/broker/broker_test.go
@@ -1,42 +1,38 @@
 package broker_test
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
 	"github.com/m110/canvas-game/pkg/broker"
 	"github.com/m110/canvas-game/pkg/game/player"
+	"github.com/m110/canvas-game/pkg/game/position"
 )
 
 func TestBroker(t *testing.T) {
 	b := broker.NewBroker()
 
-	joinedChan := b.Subscribe(player.PlayerEvent{})
-	movedChan := b.Subscribe(player.MovedEvent{})
+	playerChan := b.Subscribe(player.PlayerEvent{})
 
 	wait := make(chan struct{})
 
 	go func() {
 		for {
 			select {
-			case joinedEvent := <-joinedChan:
-				fmt.Println("Joined", joinedEvent)
-			case movedEvent := <-movedChan:
-				fmt.Println("Moved", movedEvent)
+			case <-playerChan:
+				wait <- struct{}{}
+				return
 			case <-time.After(time.Second * 1):
-				fmt.Println("timed out")
+				t.Fatal("Timed out")
 				wait <- struct{}{}
 				return
 			}
 		}
 	}()
 
-	playerJoined := player.NewJoinedEvent("player-id")
-	playerMoved := player.NewMovedEvent("player-id", player.NewPosition(1, 2))
-
+	p := player.NewPlayer("player-id", position.NewPosition(0, 0))
+	playerJoined := player.NewJoinedEvent(*p)
 	b.Publish(playerJoined)
-	b.Publish(playerMoved)
 
 	<-wait
 }
